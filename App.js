476db4dd9f81a658dbaef886801962ab385c4d2f
sheikh1906 <sheikh1906@gmail.com>
2019-11-14 21:05:29 -0600

    Adding more navigation and auth screens

--- App.js
+++ App.js
@@ -6,16 +6,19 @@
  */
 
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'; 
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
+import AuthLoadingScreen from './src/login/AuthLoadingScreen';
+import LoginScreen from './src/login/LoginScreen';
+import SignUpScreen from './src/login/SignUpScreen';
 import PoolListScreen from './src/pools/PoolListScreen';
 import ProfileScreen from './src/account/ProfileSreen';
 import TicketListScreen from './src/tickets/TicketListScreen';
 import TicketAddEditScreen from './src/tickets/TicketAddEditScreen';
 
-const AddTab = createStackNavigator(
+const AddTabStack = createStackNavigator(
   {
     Add: { screen: TicketAddEditScreen }
   },
@@ -24,16 +27,18 @@ const AddTab = createStackNavigator(
   }
 );
 
-const HomeTab = createStackNavigator(
+const HomeTabStack = createStackNavigator(
   {
     Home: { screen: TicketListScreen }
+    // Tickets for Pool
+    // Tickets for User
   },
   {
 
   }
 );
 
-const PoolsTab = createStackNavigator(
+const PoolsTabStack = createStackNavigator(
   {
     Pools: { screen: PoolListScreen }
   },
@@ -42,7 +47,7 @@ const PoolsTab = createStackNavigator(
   }
 );
 
-const SettingsTab = createStackNavigator(
+const SettingsTabStack = createStackNavigator(
   {
     Profile: { screen: ProfileScreen }
   },
@@ -51,15 +56,36 @@ const SettingsTab = createStackNavigator(
   }
 );
 
-const App = createBottomTabNavigator(
+const LoggedInTabNav = createBottomTabNavigator(  
   {
-    Add: AddTab,
-    Home: HomeTab,
-    Pools: PoolsTab,
-    Settings: SettingsTab
+    Home: HomeTabStack,
+    Add: AddTabStack,
+    Pools: PoolsTabStack,
+    Settings: SettingsTabStack
   },
   {
+    initialRouteName: 'Home'
+  }
+);
 
+const AuthStackNav = createStackNavigator(
+  {
+    Login: { screen: LoginScreen },
+    SignUp: { screen: SignUpScreen }
+  },
+  {
+    initialRouteName: 'Login'
+  }
+);
+
+const App = createSwitchNavigator(
+  {
+    AuthLoading: { screen: AuthLoadingScreen },
+    Login: AuthStackNav,
+    App: LoggedInTabNav
+  },
+  {
+    initialRouteName: 'AuthLoading'
   }
 );
 
---
+++ src/login/LoginScreen.js
@@ -0,0 +1,19 @@
+/**
+ * Pool Part Test 1 App
+ *
+ * Author: Sheikh Faridur Rahman
+ * Company: Mogul Software LLC
+ */
+
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View><Text>Login Screen</Text></View>
+    );
+}
+
+const styles = StyleSheet.create();
+
+export default LoginScreen;
